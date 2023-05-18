7676d94e356e5eb7347a74ef543565455ee3fad3
han3sui <han3sui@gmail.com>
2023-05-18 11:16:46 +0800

    fix: 面包屑调整

--- src/layout/components/app-header/index.vue
+++ src/layout/components/app-header/index.vue
@@ -1,9 +1,10 @@
 <template>
     <div class="app-header">
         <a-breadcrumb>
-            <a-breadcrumb-item v-for="item in breadList" :key="item.name" @click="onClick(item)">{{
-                item.meta.title
-            }}</a-breadcrumb-item>
+            <a-breadcrumb-item v-for="(item, index) in breadList" :key="item.name">
+                <div v-if="checkDidsable(item, index)">{{ item.meta.title }}</div>
+                <a-link v-else @click="onClick(item)"> {{ item.meta.title }}</a-link>
+            </a-breadcrumb-item>
         </a-breadcrumb>
     </div>
 </template>
@@ -11,12 +12,28 @@
 import { RouteLocationMatched } from "vue-router";
 const router = useRouter();
 const route = useRoute();
+
 const breadList = computed(() => {
     return route.matched.filter((item) => item.path !== "/" && item.meta.title);
 });
 const onClick = (item: RouteLocationMatched) => {
     router.push(item);
 };
+
+function checkDidsable(item: RouteLocationMatched & { permission?: () => boolean }, index: number) {
+    if (index === breadList.value.length - 1) {
+        return true;
+    }
+    if (!item.children?.length) {
+        return false;
+    }
+    //如果子路由全部都是隐藏的，则认为没有子路由
+    const hideRouters = item.children.filter((v: any) => v.meta?.hidden === true || v.permission?.() === false);
+    if (hideRouters.length === item.children.length) {
+        return false;
+    }
+    return true;
+}
 </script>
 <style lang="scss" scoped>
 .app-header {
