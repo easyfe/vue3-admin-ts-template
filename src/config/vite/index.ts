e777b7eb6ab09f8f95cc720d19c12f510e8263fc
han3sui <han3sui@gmail.com>
2023-06-15 17:31:11 +0800

    fix: 页面name设置

--- src/config/vite/index.ts
+++ src/config/vite/index.ts
@@ -16,6 +16,7 @@ import { ConfigOssPlugin } from "./modules/oss";
 import { CosConfig } from "@syyfe/vite-plugin-cos";
 import VueSetupExtend from "vite-plugin-vue-setup-extend-plus";
 import { vitePluginForArco } from "@arco-plugins/vite-vue";
+import DefineOptions from "unplugin-vue-define-options/vite";
 
 /**
  * 创建vite插件
@@ -44,10 +45,11 @@ export function createVitePlugins(params: {
          * 扩展setup插件，支持在script标签中使用name属性
          * usage: <script setup name="MyComp"></script>
          */
-        VueSetupExtend(),
+        // VueSetupExtend(),
         vitePluginForArco({
             theme: "@arco-themes/vue-easyfe"
-        })
+        }),
+        DefineOptions()
     ];
     // 上传oss
     if (params.envMap.VITE_APP_MODE !== "development" && params.uploadOption?.SecretId) {
--- src/layout/main.vue
+++ src/layout/main.vue
@@ -5,15 +5,11 @@
         </keep-alive>
     </router-view>
 </template>
-<script lang="ts">
-const name = "1231";
-export default {
-    name
-};
-</script>
 <script setup lang="ts" name="LayoutMain">
 import routes from "@/config/pinia/routes";
-
+defineOptions({
+    name: "1231321"
+});
 const keepList = computed(() => {
     return routes().cachedTags;
 });
