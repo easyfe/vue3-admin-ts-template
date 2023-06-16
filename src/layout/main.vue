2c9782a130c003fcaad8fbb53fea3e23ff0a1582
han3sui <han3sui@gmail.com>
2023-06-16 10:32:41 +0800

    fix: 依赖升级

--- src/layout/main.vue
+++ src/layout/main.vue
@@ -6,14 +6,13 @@
     </router-view>
 </template>
 <script lang="ts">
-const name = "1231";
+const name = window.location.pathname.split("/").join("_");
 export default {
     name
 };
 </script>
-<script setup lang="ts" name="LayoutMain">
+<script setup lang="ts">
 import routes from "@/config/pinia/routes";
-
 const keepList = computed(() => {
     return routes().cachedTags;
 });
