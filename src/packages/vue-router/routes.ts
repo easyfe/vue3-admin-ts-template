8467b6a0fc4ba7399cd796e070690958ba1d3182
han3sui <han3sui@gmail.com>
2023-06-16 14:34:52 +0800

    fix: 路由优化

--- src/packages/vue-router/routes.ts
+++ src/packages/vue-router/routes.ts
@@ -2,10 +2,9 @@
  * @Author: hanxinxin
  * @Date: 2021-04-21 13:00:54
  * @Last Modified by: hanxinxin
- * @Last Modified time: 2023-06-16 10:43:11
+ * @Last Modified time: 2023-06-16 14:34:35
  */
 import { RouteRecordRaw } from "vue-router";
-import baseRouter from "./base";
 import env from "@/utils/tools/env";
 
 const modulesFiles: Record<string, any> = import.meta.globEager("/src/config/router/*.ts");
@@ -25,7 +24,5 @@ for (const key in modules) {
 if (modules?.dev && env.dev()) {
     routers.push(...modules.dev);
 }
-
-routers.push(...baseRouter);
 
 export default routers as RouteRecordRaw[];
