fc8d8eac4d38d0c801d9dd41a7d6fbf7b4ede723
han3sui <han3sui@gmail.com>
2023-06-19 10:13:24 +0800

    fix: 三级路由父组件异常处理

--- src/layout/main.vue
+++ src/layout/main.vue
@@ -5,6 +5,12 @@
         </keep-alive>
     </router-view>
 </template>
+<script lang="ts">
+const name = window.location.pathname.split("/").join("_");
+export default {
+    name
+};
+</script>
 <script setup lang="ts">
 import routes from "@/config/pinia/routes";
 const keepList = computed(() => {
